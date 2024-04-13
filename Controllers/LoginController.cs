35c3a7aba6fbf8a13fe12296165d9883884cb34a
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-04-13 13:04:28 +0200

    Add Login/Register Page Controller Botsrapp/  Changed BAcground and navbar

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WWW_APP_PROJECT.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
