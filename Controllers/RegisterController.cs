4b5f678f49dc8341dd211eb8effcf038d6c4a11b
FireFly0707 <jakubswietliczny@gmail.com>
2024-05-08 09:13:39 +0200

    podpięto login i register

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp_tutorial.Data;
+using WWW_APP_PROJECT.Models;
+using WWW_APP_PROJECT.ViewModels;
 
 namespace WWW_APP_PROJECT.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public RegisterController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use.";
+                return View(registerViewModel);
+            }
+            var newUser = new AppUser
+            {
+                UserName = registerViewModel.UserName,
+                Email = registerViewModel.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            else
+            {
+                TempData["Error"] = "Password is too weak";
+                return View(registerViewModel);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
