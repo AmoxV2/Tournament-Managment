e34786e30b1a740afeac361e9dff737f6f63ce71
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-05-09 10:53:07 +0200

    Dodanie Dashborda

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//Sidebar TOGGLE 
+var sidebarOpen = false;
+var sidebar = document.getElementById('sidebar');
+
+function opneSidebar()
+{
+    if(!sidebarOpen)
+    {
+        sidebar.classList.add('sidebar_responsive');)
+        sidebarOpen = true;
+    })
+}
+
+
+function closeSidebar()
+{ 
+    if(sidebarOpen)
+    {
+        sidebar.classList.remove('sidebar_responsive');
+        sidebarOpen = false;
+    })
+
+}
